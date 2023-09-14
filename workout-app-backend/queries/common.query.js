a67b09f03f933309012ae09bfbbd5a1b99c65b6a
retro1000 <136360020+retro1000@users.noreply.github.com>
2023-09-15 00:50:17 +0530

    Delete workout-app-backend directory

--- workout-app-backend/queries/common.query.js
+++
@@ -1,9 +0,0 @@
-const generateUniqueId = (existingIds, candidateId) => {
-    try{
-        return (existingIds.includes(candidateId))?generateUniqueId(existingIds, candidateId + 1):candidateId;
-    }catch(err){
-        throw err;
-    }
-};
-
-export const CommonQueries = {generateUniqueId};
