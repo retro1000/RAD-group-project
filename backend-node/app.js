1c7abf3ae6fa8c1fac9a85f0e19053db157bf707
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2023-09-15 02:07:48 +0530

    Add luwin branch

--- backend-node/app.js
+++ backend-node/app.js
@@ -14,11 +14,10 @@ app.use("/notice", router);
 app.use("/user", userRouter);
 app.use("/Lecture", LectureRouter);
 
-// app.use("/libarary", libraryItemRouter);
-//app.use("/resource",resour )
+
 app.use(cors());
 app.use("/pdf", require("./Router/LibraryItemRouter"));
-// app.use("/lecture", require("./Router/LectureRouter"));
+
 
 mongoose
   .connect(
